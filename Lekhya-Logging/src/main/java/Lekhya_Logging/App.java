210818aeccf0077bd3f39b2495e5aec64aea324f
LekhyaSahithiP <lelkyasahithipanchumarthi@gmail.com>
2020-02-26 18:39:44 +0530

    Lekhya Epam task-5 Logging

---
+++ Lekhya-Logging/src/main/java/Lekhya_Logging/App.java
@@ -0,0 +1,13 @@
+package Lekhya_Logging.Lekhya_Logging;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
